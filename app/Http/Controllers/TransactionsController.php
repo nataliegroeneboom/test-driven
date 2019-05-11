b8765832d8d4f82d41f47cb3ed89b1dd23089a2a
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-05-11 10:54:54 +0100

    add transaction model pass transactions variable to view

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Transaction; 
+ 
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionsController extends Controller
 {
     public function index(){
         $transactions = Transaction::all();
-        return view('transactions.index');
+        return view('transactions.index', compact('transactions'));
     }
 }
