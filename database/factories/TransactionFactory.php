0c40a5abb12d3bffd1a512117dba780e3cf5a353
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-05-14 06:48:06 +0100

    created tests and category model with migration and factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Category;
 use App\Transaction;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -8,6 +9,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Transaction::class, function (Faker $faker) {
     return [
-      'description' => $faker->sentence(2)
+      'description' => $faker->sentence(2),
+      'category_id' => function() {
+          return factory(App\Category::class)->create()->id;
+      }
     ];
+});
+
+$factory->define(App\Category::class, function (Faker $faker) {
+  return [
+    'name' => $faker->word
+  ];
 });
